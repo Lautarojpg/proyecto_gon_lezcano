c7f5c88afb2232a5088857f70c9a01643cb7bca2
Lautaro Lezcano <lautarofranciscolezcano@gmail.com>
2025-04-21 21:54:29 -0300

    cambios de las recomendaciones

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -1,2 +1,17 @@
+<<<<<<< HEAD
 <!--cabecera-->
 <title><?php echo($titulo);?></title>
+=======
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+        <link href="assets\css\miestilo.css" rel="stylesheet">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="C:\xampp\htdocs\proyecto_gon_lezcano\assets\js\miJs.js"></script>
+        <link rel="icon" href="assets/img/logo.png" type="image/x-icon">
+        <title>PepePelis</title>
+    </head>
+<body>
+>>>>>>> 57be2c4 (cambios de las recomendaciones)
--- app/Views/front/inicio.php
+++ app/Views/front/inicio.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,6 +9,9 @@
     <body>
         <!-- Carrusel -->
         <section class="container" style="width: 50%">
+=======
+<section class="container">
+>>>>>>> 57be2c4 (cambios de las recomendaciones)
             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -34,6 +38,7 @@
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+<<<<<<< HEAD
         </section>
         <section> <!--cards-->
             <div class="row"> <!-- primera comluna-->
@@ -127,3 +132,76 @@
         </section>
     </body>
 </html>
+=======
+</section>
+<section> <!--cards-->
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <!-- Primer slide -->
+    <div class="carousel-item active">
+      <div class="d-flex justify-content-around">
+        <div class="card" style="width: 18rem;">
+          <img src="assets/img/20000Leagues.jpg" class="card-img-top" alt="20000 Leguas">
+          <div class="card-body">
+            <h5 class="card-title">20.000 Leguas de Viaje Submarino</h5>
+            <p class="card-text">Un barco enviado a investigar varios hundimientos misteriosos se encuentra un submarino, Nautilus, comandado por el capitán Nemo.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="assets/img/battleOfTheWorlds.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">La Batalla de los Mundos</h5>
+            <p class="card-text">Un pequeño pueblo de California es atacado por marcianos, lo que da comienzo a una invasión mundial.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="assets/img/duckSoup.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">Duck Soup</h5>
+            <p class="card-text">Rufus T. Firefly es embestido presidente dictatorial de la nación en bancarrota Libertonia y declara la guerra a la vecina Sylvania por el amor de la rica viuda Gloria Teasdale.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Segundo slide -->
+    <div class="carousel-item">
+      <div class="d-flex justify-content-around">
+        <div class="card" style="width: 18rem;">
+          <img src="assets/img/houseOnHauntedHill.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">House on Haunted Hill</h5>
+            <p class="card-text">Un millonario ofrece diez mil dólares a cinco personas que accedan a permanecer encerradas en una casa grande y espeluznante durante una noche con él y su esposa.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="assets/img/myManGodfrey.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">My Man Godfrey</h5>
+            <p class="card-text">La rica heredera Irene contrata a Godfrey como mayordomo familiar sin saber de su misterioso pasado.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="assets/img/nosferatu.jpg" class="card-img-top" alt="Nosferatu">
+          <div class="card-body">
+            <h5 class="card-title">Nosferatu</h5>
+            <p class="card-text">El vampiro conde Orlok manifiesta su interés por una nueva residencia y por la esposa del agente inmobiliario Hutter.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controles del carrusel -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</section>
+>>>>>>> 57be2c4 (cambios de las recomendaciones)
