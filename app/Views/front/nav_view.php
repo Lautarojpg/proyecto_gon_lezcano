b1af4133e51deb937fd51055dc2a1a81f2f6ea0e
Lautaro Lezcano <lautarofranciscolezcano@gmail.com>
2025-04-23 10:12:02 -0300

    cambio de imagenes de comercializacion y arreglo del dropbox

--- app/Views/front/nav_view.php
+++ app/Views/front/nav_view.php
@@ -16,20 +16,24 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
+<<<<<<< HEAD
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
+=======
+                    <a class="nav-link active" href="<?= base_url('quienes_somos') ?>">Nosotros</a>
+>>>>>>> d3879e4 (cambio de imagenes de comercializacion y arreglo)
                     </li>
-                    <!--<li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Generos
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="catalogo#terror">Terror</a></li>
-                        <li><a class="dropdown-item" href="catalogo#ciencia_ficcion">Ciencia Ficcion</a></li>
-                        <li><a class="dropdown-item" href="catalogo#comedia">Comedia</a></li>
-                    </ul>-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="<?= base_url('catalogo') ?>" id="catalogoDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Catálogo
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="catalogoDropdown">
+                            <li><a class="dropdown-item" href="<?= base_url('catalogo#terror') ?>">Terror</a></li>
+                            <li><a class="dropdown-item" href="<?= base_url('catalogo#ciencia_ficcion') ?>">Ciencia Ficción</a></li>
+                            <li><a class="dropdown-item" href="<?= base_url('catalogo#comedia') ?>">Comedia</a></li>
+                        </ul>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link active" href="<?= base_url('comercializacion') ?>">Comercializacion</a>
@@ -42,5 +46,6 @@
                 </div>
             </div>
         </nav>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
